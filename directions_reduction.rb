1a9073fdec2e7c83703a47dee36a978b160e364d
ravenvn <ravenvn@yahoo.com.vn>
2017-06-07 16:41:00 +0700

    pass tests

---
+++ directions_reduction.rb
@@ -0,0 +1,29 @@
+def dirReduc(arr)
+  b = Hash.new
+  b['NORTH'] = 'SOUTH'
+  b['SOUTH'] = 'NORTH'
+  b['EAST'] = 'WEST'
+  b['WEST'] = 'EAST'
+  
+  flag_while = true
+
+  while flag_while do
+    flag_for = true
+    return arr if arr.length < 2
+
+    for i in 0..(arr.length-2)
+      if arr[i] == b[arr[i+1]]
+        arr.slice!(i..(i+1))
+        flag_for = false
+      end
+
+      break if flag_for == false
+      flag_while = false if i == arr.length-2
+    end
+  end
+  return arr
+end
+
+a = ["NORTH", "WEST", "SOUTH", "EAST"]
+# a = ["NORTH", "SOUTH"]
+puts dirReduc(a)
